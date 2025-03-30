4ae9bba177d5ea17149d2ed554e2d84c65bfd62e
Jefferson Rocha <jeffersonrocha@MacBook-Air-de-Jefferson.local>
2025-03-29 23:55:39 -0300

    tela home

---
+++ src/pages/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <span class="title">Meu quadro de tarefas</span>
+    </div>
+  </div>
+</template>
+<script setup></script>
+
+<style scoped>
+.home {
+  background: #ffffff;
+  width: 100%;
+  height: 100vh;
+}
+.title {
+  font-size: 50px;
+  font-weight: 700;
+  color: #000;
+  font-family: sans-serif;
+}
+</style>
