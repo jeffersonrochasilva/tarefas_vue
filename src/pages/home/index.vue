8ea600309f574f312a9462b690327a0deb10ddba
Jefferson Rocha <jeffersonrocha@MacBook-Air-de-Jefferson.local>
2025-03-30 23:47:26 -0300

    titulo

---
+++ src/pages/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+      <span class="title">Meu quadro de tarefas</span>
+    </div>
+  </div>
+</template>
+<script setup></script>
+
+<style scoped>
+.home {
+  background: #ffffff;
+  width: 100%;
+  height: 100vh;
+}
+.title {
+  font-size: 50px;
+  font-weight: 700;
+  color: #000;
+  font-family: sans-serif, Nunito;
+}
+</style>
